0693a6503f467b584f8783c16a5c8b3e9e96c853
LEE DONG GYU <tenenger7125@gmail.com>
2024-07-30 16:08:13 +0900

    refactor: add eslint.env and eslint.parseOptions

--- packages/eslint-config-dotori-react/index.js
+++ packages/eslint-config-dotori-react/index.js
@@ -1,4 +1,13 @@
 module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'es2020',
+    sourceType: 'module',
+  },
   extends: [
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
