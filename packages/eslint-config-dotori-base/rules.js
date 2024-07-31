b95e4e179c3981483df0a15deaa758df59de913e
LEE DONG GYU <tenenger7125@gmail.com>
2024-07-31 14:04:20 +0900

    [eslint-config-dotori-base@fix]: add rules attribute

--- packages/eslint-config-dotori-base/rules.js
+++ packages/eslint-config-dotori-base/rules.js
@@ -1,20 +1,22 @@
 module.exports = {
-  'import/extensions': [
-    'error',
-    'never',
-    {
-      svg: 'always',
-      ttf: 'always',
-      css: 'always',
-    },
-  ],
-  'no-underscore-dangle': 'off',
-  'no-use-before-define': 'off',
-  'no-shadow': 'off',
-  'no-nested-ternary': 'off',
-  'import/no-unresolved': 'off',
-  'import/no-cycle': 'off',
-  'import/no-extraneous-dependencies': 'off',
-  'no-restricted-syntax': ['error', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
-  'import/prefer-default-export': 'off',
+  rules: {
+    'import/extensions': [
+      'error',
+      'never',
+      {
+        svg: 'always',
+        ttf: 'always',
+        css: 'always',
+      },
+    ],
+    'no-underscore-dangle': 'off',
+    'no-use-before-define': 'off',
+    'no-shadow': 'off',
+    'no-nested-ternary': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-cycle': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-restricted-syntax': ['error', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
+    'import/prefer-default-export': 'off',
+  },
 };
