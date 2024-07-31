3f8e6de63f5e8258dca791b88adb341c781e9e07
LEE DONG GYU <tenenger7125@gmail.com>
2024-07-31 14:03:36 +0900

    [eslint-config-dotori-react@delete]: delete deplicated extend

--- packages/eslint-config-dotori-react/index.js
+++ packages/eslint-config-dotori-react/index.js
@@ -9,7 +9,6 @@ module.exports = {
     sourceType: 'module',
   },
   extends: [
-    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
     'plugin:react/jsx-runtime',
