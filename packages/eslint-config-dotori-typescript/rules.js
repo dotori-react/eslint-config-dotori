fdd24d040e08fb05d9bb99558d139195ae5429d5
LEE DONG GYU <tenenger7125@gmail.com>
2024-08-03 03:30:49 +0900

    [eslint-config-dotori-typescript off import/no-extraneous-dependencies rule]

--- packages/eslint-config-dotori-typescript/rules.js
+++ packages/eslint-config-dotori-typescript/rules.js
@@ -2,6 +2,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
+    'import/no-extraneous-dependencies': 'off',
 
     // eslint-config-airbnb-typescript rule off
     'react/jsx-filename-extension': 'off',
