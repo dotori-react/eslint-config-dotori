79dd7a474608c1a84264c4b3af1396a113e7cb44
LEE DONG GYU <tenenger7125@gmail.com>
2024-08-06 12:15:59 +0900

    [eslint-config-dotori-typescript@fix]: turn off rules
     - @typescript-eslint/lines-between-class-members
    
     - @typescript-eslint/no-throw-literal

--- packages/eslint-config-dotori-typescript/rules.js
+++ packages/eslint-config-dotori-typescript/rules.js
@@ -3,6 +3,8 @@ module.exports = {
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
     'import/no-extraneous-dependencies': 'off',
+    '@typescript-eslint/lines-between-class-members': 'off',
+    '@typescript-eslint/no-throw-literal': 'off',
 
     // eslint-config-airbnb-typescript rule off
     'react/jsx-filename-extension': 'off',
