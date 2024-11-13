c9013182ef805bab0eeaf9a075793c58cdd8e838
LEE DONG GYU <tenenger7125@gmail.com>
2024-11-13 12:47:32 +0900

    [eslint-config-dotori-typescript@feat]: add non-check void return when use Promise

--- packages/eslint-config-dotori-typescript/rules.js
+++ packages/eslint-config-dotori-typescript/rules.js
@@ -5,6 +5,12 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     '@typescript-eslint/lines-between-class-members': 'off',
     '@typescript-eslint/no-throw-literal': 'off',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: false,
+      },
+    ],
 
     // eslint-config-airbnb-typescript rule off
     'react/jsx-filename-extension': 'off',
