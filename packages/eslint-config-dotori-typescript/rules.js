ef12383f721f5c6aa48ef18884dcebfb1fcb2710
LEE DONG GYU <tenenger7125@gmail.com>
2024-11-27 17:57:04 +0900

    [eslint-config-dotori-import-sort@feat]: add to delete unused-imported-vars

--- packages/eslint-config-dotori-typescript/rules.js
+++ packages/eslint-config-dotori-typescript/rules.js
@@ -12,6 +12,15 @@ module.exports = {
       },
     ],
 
+    // delete unused-imported-vars
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+    ],
+
     // eslint-config-airbnb-typescript rule off
     'react/jsx-filename-extension': 'off',
   },
