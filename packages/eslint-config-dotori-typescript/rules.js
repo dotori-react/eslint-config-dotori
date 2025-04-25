dc98ad44d5803e362310bdc65fffc3c94fc55172
LEE DONG GYU <tenenger7125@gmail.com>
2025-04-25 09:36:49 +0900

    [eslint-config-dotori-typescript@feat]: modify no-unused-vars rule

--- packages/eslint-config-dotori-typescript/rules.js
+++ packages/eslint-config-dotori-typescript/rules.js
@@ -14,7 +14,18 @@ module.exports = {
 
     // delete unused-imported-vars
     'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'all',
+        argsIgnorePattern: '^_',
+        caughtErrors: 'all',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
     'unused-imports/no-unused-imports': 'error',
     'unused-imports/no-unused-vars': [
       'warn',
